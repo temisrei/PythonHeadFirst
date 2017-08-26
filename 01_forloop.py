7053f576bda83e8332a2396cdfac668a90f131ff
Rei Lin <temisrei@gmail.com>
2017-08-26 11:43:52 +0800

    加入判斷項目是否為清單

--- 01_forloop.py
+++ 01_forloop.py
@@ -2,4 +2,8 @@ movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Ch
 # print(movies)
 
 for each_item in movies:
-  print(each_item)
+  if (isinstance(each_item, list)):
+    for nested_item in each_item:
+      print(nested_item)
+  else:
+    print(each_item)

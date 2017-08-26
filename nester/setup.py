372a44cd48f99b49a5aca36a38fbc4b02bd2c582
Rei Lin <temisrei@gmail.com>
2017-08-26 14:57:42 +0800

    python setup.py register sdist upload -r https://www.python.org/pypi

---
+++ nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name = 'rei_nester',
+  version = '1.0.0',
+  py_modules = ['nester'],
+  author = 'temisrei',
+  author_email = 'temisrei@gmail.com',
+  url = 'https://temisrei.github.io',
+  description = 'A simple printer of nested lists',
+)

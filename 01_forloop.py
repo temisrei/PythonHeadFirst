4ee4925ef00be035d3cbb550765c3ce1ec1cfe71
Rei Lin <temisrei@gmail.com>
2017-08-26 15:07:37 +0800

    修正 nester 新增 level 參數使得列印有層次。

--- 01_forloop.py
+++ 01_forloop.py
@@ -3,5 +3,6 @@ import nester
 # movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman", ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 # print(movies)
 
-casts = ['Palin','Cleese','Idle','Jones','Gilliam','Chapman']
-nester.print_lol(casts)
+# casts = ['Palin','Cleese','Idle','Jones','Gilliam','Chapman']
+casts = ['John','Eric',['Cleese','Idle'], 'Michael', ['Palin']]
+nester.print_lol(casts, 0)
--- nester/nester.py
+++ nester/nester.py
@@ -3,12 +3,14 @@
 練習建立模組並上傳至 PyPI。
 """
 
-def print_lol(the_list):
+def print_lol(the_list, level):
   """
   需傳入 清單 list，名稱為 the_list 
   """
   for each_item in the_list:
     if isinstance(each_item, list):
-      print_lol(each_item)
+      print_lol(each_item, level+1)
     else:
+      for tab_stop in range(level):
+        print("\t", end='')
       print(each_item)

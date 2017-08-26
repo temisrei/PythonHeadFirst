13c9823cbaa9981f2f8c778a012600d8226aef00
Rei Lin <temisrei@gmail.com>
2017-08-26 11:47:51 +0800

    建立遞迴函式

--- 01_forloop.py
+++ 01_forloop.py
@@ -1,9 +1,11 @@
 movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman", ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
 # print(movies)
 
-for each_item in movies:
-  if (isinstance(each_item, list)):
-    for nested_item in each_item:
-      print(nested_item)
-  else:
-    print(each_item)
+def print_lol(the_list):
+  for each_item in the_list:
+    if (isinstance(each_item, list)):
+      print_lol(each_item)
+    else:
+      print(each_item)
+
+print_lol(movies)
